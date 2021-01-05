ae06fd9b40f3b7cfa10385d52189dc4f9349356b
hackettyu <hy.lobby@gmail.com>
2021-01-05 23:26:18 +0800

    Fixed: crontab expression & script

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const fetch = () => axios.get('https://s.weibo.com/top/summary').then(res => {
 })
 
     ; (async () => {
-        const { list, desc } = await fetch()
+        const list, desc = await fetch()
         const gist = await octokit.gists.get({ gist_id: gistId }).catch(error => {
             console.error('Cannot update gist.')
             throw error
